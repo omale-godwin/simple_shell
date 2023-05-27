c023dce9d95888552c72b768de6afd1115302b33
dandyson2 <dandysonvictor@yahoo.com>
2023-05-26 23:26:53 -0700

    Betty would be proud

---
+++ betty_shell.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+#include <stdio.h>
+
+int main(void)
+{
+	int m = 5;
+	int n = 10;
+	int add = m + n;
+
+	printf("The total of %d and %d is %d\n", m, n, add);
+
+	return (0);
+}
